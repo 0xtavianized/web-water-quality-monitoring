13a17eaf48fd385c6059690f1dfd6fe614a5fa7b
0xtavianized <oktavian@stmik-wp.ac.id>
2024-12-17 21:32:43 +0700

    Fix mobile view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,7 +23,7 @@
                 </div>
             </div>
             <div id="hs-navbar-example" class="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block" aria-labelledby="hs-navbar-example-collapse">
-                <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+                <div class="flex flex-col gap-5 mt-5 items-end sm:flex-row sm:items-center sm:justify-end sm:ps-5">
                     <a class="font-medium text-blue-500 focus:outline-none" href="{{ url('/') }}" aria-current="page">Home</a>
                     <a class="font-medium text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500" href="{{ url('/history') }}">History</a>
                 </div>
