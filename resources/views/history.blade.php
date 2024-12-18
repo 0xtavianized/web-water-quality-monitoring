cf5562f52dea4e9b4305ed143ba4691945325f66
0xtavianized <oktavian@stmik-wp.ac.id>
2024-12-18 23:53:39 +0700

    Fix dark mode and mobile view

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -32,7 +32,7 @@
         </nav>
     </header>
 
-    <div class="w-full h-screen dark:bg-neutral-800">
+    <div class="w-full dark:bg-neutral-800">
     <div class="flex flex-col bg-white border border-gray-200 shadow-sm rounded-xl p-4 md:p-5 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-800">
         <div class="flex space-x-3 w-full">
             <div class="sm:flex sm:items-center w-1/2">
