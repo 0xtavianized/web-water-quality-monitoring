d712382791ba3396110cb64d42445b40372937a7
0xtavianized <oktavian@stmik-wp.ac.id>
2024-12-19 00:22:58 +0700

    Fix height navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <header class="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3 dark:bg-neutral-900 sticky top-0 z-50">
+    <header class="flex flex-wrap h-20 sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3 dark:bg-neutral-900 sticky top-0 z-50">
         <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
             <div class="flex items-center justify-between">
                 <a class="flex-none text-xl font-semibold dark:text-white focus:outline-none focus:opacity-80" href="#" aria-label="Brand">
